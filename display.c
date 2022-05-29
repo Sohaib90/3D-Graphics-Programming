e5438c2d6beb5faf42a95e3abc18cef4af4aada4
Sohaib90 <sohaib.arshid101@gmail.com>
2022-05-29 15:58:51 +0200

    Adding dynamic array

--- display.c
+++ display.c
@@ -118,10 +118,13 @@ void draw_line(int x0, int y0, int x1, int y1, uint32_t color) {
 	}
 }
 
+void draw_bres_line(int x1, int y1, int x2, int y2, uint32_t color) {
+}
+
 void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
 	draw_line(x0, y0, x1, y1, color);
-	draw_line(x1, y1, x2, y2, color); 
-	draw_line(x0, y0, x2, y2, color);	
+	draw_line(x1, y1, x2, y2, color);
+	draw_line(x0, y0, x2, y2, color);
 }
 
 void render_color_buffer() {
